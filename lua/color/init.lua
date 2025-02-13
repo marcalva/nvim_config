e822cdb6328e19bbe2ee01ad2cfe2b30c39425dd
Marcus Alvarez <marcus.alvarez@ucsf.edu>
2025-02-12 23:18:07 -0800

    more contrast

--- lua/color/init.lua
+++ lua/color/init.lua
@@ -1,4 +1,6 @@
 --- Colors
 vim.opt.background="dark"
 vim.o.termguicolors = true
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_foreground = "material"
 vim.cmd[[colorscheme gruvbox-material]]
