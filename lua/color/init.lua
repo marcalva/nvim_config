864798fbd258e433b95b8b9ee0a622568caf1505
Marcus Alvarez <marcus.alvarez@ucsf.edu>
2025-06-09 17:46:28 -0700

    tmux slime

--- lua/color/init.lua
+++ lua/color/init.lua
@@ -3,4 +3,4 @@ vim.opt.background="dark"
 vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_transparent_background = 1
 vim.g.gruvbox_material_foreground = "material"
-vim.cmd[[colorscheme PaperColor]]
+vim.cmd[[colorscheme gruvbox-material]]
